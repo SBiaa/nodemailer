b3bc4ef959f7e7d8ea53bd6da4eb1fc9b4d39074
SBiaa <developer.biaa@gmail.com>
2023-01-26 13:53:52 -0300

    updade nodemailer

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const nodemailer = require('nodemailer');
 
-const transport = nodemailer.createTransport({
-    host: '',
-    PORT: 0,
-    secure: false,
-    auth: {
-        user:'',
-        pass:'',
-    }
-});
-
+    const transport = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        PORT: 587,
+        secure: false,
+        auth: {
+            user:'',
+            pass:'',
+        }
+    });
 
 transport.sendMail({
     from: '',
     to: '',
-    subject: '',
-    html:'<>',
-    text:'',
-})
+    subject: 'Enviando email com Nodemailer',
+    html:'<h1>Oii, tudo bem?</h1> <p> Esse email foi enviando com Nodemailer </p> <p> Caso você seja um(a) recrutador(a), saiba que eu sou muito esforçada e amo aprender coisas novas, me de um voto de confiança para poder mostrar o meu potencial </p><p> Caso você seja um(a) dev procurando inspirações, me manda uma mensagem e vamos pensar em um projeto legal para desenvolver</p>',
+    text:'Oii, tudo bem?\n  Esse email foi enviando com Nodemailer \n \n Caso você seja um(a) recrutador(a), saiba que eu sou muito esforçada e amo aprender coisas novas, me de um voto de confiança para poder mostrar o meu potencial\n \n  Caso você seja um(a) dev procurando inspirações, me manda uma mensagem e vamos pensar em um projeto legal para desenvolver',
+}).then(() => console.log('Email enviado com sucesso!'))
+.catch((err) => console.log("Erro ao enviar email", err))
